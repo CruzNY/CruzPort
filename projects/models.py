b265a85b78900eb98cb45835bd6685fc49dac18c
Alex Cruz <Alexcruz182@gmail.com>
2018-06-14 21:43:22 -0400

    Created models, superUser, edited base template and created projects view / html

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
 # Create your models here.
+
+#Creating Objects for our database
+class project(models.Model):
+    title = models.CharField(max_length=50)
+    authors = models.CharField(max_length=80)
+    submission_date = models.DateTimeField() 
+    image = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=100)
+    full_body = models.TextField()
+    
--- projects/views.py
+++ projects/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'projects/home.html')
+def project(request):
+    return render(request, 'projects/project.html')

429e83a2301a30fce9296b75f0f04add60f74f3d
Alex Cruz <Alexcruz182@gmail.com>
2018-06-14 22:52:42 -0400

    project page generation

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -21,5 +21,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home,  name='home'),
-    path('projects/',views.project, name='project'),
+    path('projects/',views.pro, name='project'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import project
 
 def home(request):
     return render(request, 'projects/home.html')
-def project(request):
-    return render(request, 'projects/project.html')
+def pro(request):
+    projects = project.objects
+    return render(request, 'projects/project.html', {'projects':projects})
